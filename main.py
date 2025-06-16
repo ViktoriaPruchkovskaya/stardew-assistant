658c0639d429ec13df471529c9c94b25eb600593
ViktoriaPruchkovskaya <pruchkovskaya@pm.me>
2025-06-16 15:34:58 -0700

    Format code and organize imports

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import asyncio
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+
+from server.mcp_client import Config, MCPClient
+
 load_dotenv()
-from server.mcp_client import MCPClient, Config
-            
+
 
 async def main():
     config: Config = Config(
         version=os.getenv("API_VERSION"),
         endpoint=os.getenv("ENDPOINT"),
         api_key=os.getenv("SUBSCRIPTION_KEY"),
-        deployment=os.getenv("DEPLOYMENT")
-        )
+        deployment=os.getenv("DEPLOYMENT"),
+    )
     client = MCPClient(config)
     await client.connect("./mcp/main.py")
-        
+
+
 if __name__ == "__main__":
     asyncio.run(main())
--- wiki_crawler.py
+++ wiki_crawler.py
@@ -1,15 +1,16 @@
 import asyncio
 from collections import deque
-from typing import Tuple, Set
+from typing import Set, Tuple
 
-from bs4 import BeautifulSoup
 import httpx
+from bs4 import BeautifulSoup
+
 
 async def crawl_wiki(entrypoint: str):
-    queue: deque[str] = deque([entrypoint]) 
+    queue: deque[str] = deque([entrypoint])
     ignored: list[str] = ["Stardew Valley Wiki", "Getting Started"]
     visited: Set[str] = set()
-    
+
     while len(queue) > 0:
         curr = queue.popleft()
         print(f"Visiting {curr}...")
@@ -21,33 +22,33 @@ async def crawl_wiki(entrypoint: str):
                 print(f"{title} -> {href}")
                 visited.add(title)
                 queue.append(href)
-                
+
     with open("pages.csv", "w") as file:
         file.write(",".join(visited))
-        
-        
-        
-async def make_wiki_request(url: str) -> list[Tuple[str,str]] | None:
+
+
+async def make_wiki_request(url: str) -> list[Tuple[str, str]] | None:
     """Make a request to the Stardew Valley wiki"""
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
-        "Accept": "text/html,application/xhtml+xml,application/xml"
+        "Accept": "text/html,application/xhtml+xml,application/xml",
     }
     async with httpx.AsyncClient() as client:
         try:
             response = await client.get(url, headers=headers, timeout=30.0)
             response.raise_for_status()
             soup = BeautifulSoup(response.content, "html.parser")
-    
+
             link_tags = soup.select("#mw-whatlinkshere-list li a")
-            linked_pages: list[Tuple[str,str]] = []
-            for tag in link_tags: 
+            linked_pages: list[Tuple[str, str]] = []
+            for tag in link_tags:
                 href = tag.get("href")
                 title = tag.get("title")
-                if href and title and ":" not in href and 'mediawiki' not in href:
+                if href and title and ":" not in href and "mediawiki" not in href:
                     linked_pages.append((href, title))
             return linked_pages
         except Exception:
             return None
-        
+
+
 asyncio.run(crawl_wiki("/Getting_Started"))
