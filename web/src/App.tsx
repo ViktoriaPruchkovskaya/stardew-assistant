43e048f1cc6869befe965250e584eff7a2aa3600
ViktoriaPruchkovskaya <pruchkovskaya@pm.me>
2025-06-24 15:39:38 -0700

    Add logo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,14 @@
 import Dialog from "./components/Dialog";
+import image from 'url:./assets/stardew-valley-assistant.png';
 
 export default function MyApp() {
     return (
         <div className="min-h-screen bg-cover bg-center bg-no-repeat bg-[url(./assets/background-light.png)]">
             <div className="m-auto max-w-5xl px-8 py-6 flex flex-col gap-2 " >
-                < h1 className="text-center font-press-start text-3xl font-bold text-amber-50" >Stardew Valley Assistant</h1 >
+                <img
+                    src={image}
+                    className="w-lg mx-auto drop-shadow-lg"
+                />
                 <Dialog />
             </div >
         </div>
