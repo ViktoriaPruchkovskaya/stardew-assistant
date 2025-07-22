f4e47b56bcbee9fdbc50924ddd8e7ce11d013c30
ViktoriaPruchkovskaya <pruchkovskaya@pm.me>
2025-07-21 18:20:21 -0700

    reuse settings button to close settings

--- web/src/components/Settings/SettingsWindow.tsx
+++ web/src/components/Settings/SettingsWindow.tsx
@@ -1,29 +1,35 @@
 import { useContext } from "react"
 import { ThemeContext } from "../../contexts/themeContext"
+import SettingsButton from "./SettingsButton"
 
 interface SettingsWindowProps {
-    onSettingsSave: () => void
+    onSettingsClose: () => void
 }
 
-export default function SettingsWindow({ onSettingsSave }: SettingsWindowProps) {
+export default function SettingsWindow({ onSettingsClose }: SettingsWindowProps) {
     const { isDark, setIsDark } = useContext(ThemeContext)!
 
     return (
-        <div className="absolute w-100 bg-amber-100 border-4 border-yellow-700 rounded-lg shadow-lg px-4 py-3 font-press-start text-lg text-yellow-900 z-50">
-            <h2 className="text-xl mb-2 text-center">Settings</h2>
-            <label className='flex justify-start text-sm'>
-                <span>Dark theme</span>
-                <div onClick={() => setIsDark(prev => !prev)} className='bg-amber-400 w-14 h-6 border-1 border-yellow-600 rounded-xs cursor-pointer'>
-                    <div className={`w-5.5 h-5.5 bg-yellow-900 rounded-xs transition-transform duration-300 ${isDark ? 'translate-x-8' : 'translate-x-0'}`} />
+        <div className="absolute z-50">
+            <div className="relative">
+                <div className="absolute -top-15 -right-0 z-10">
+                    <SettingsButton handleOnClick={onSettingsClose}><span className="font-press-start text-xl">X</span></SettingsButton>
                 </div>
-            </label>
-            <div className='flex justify-center'>
-                <button type='button' onClick={onSettingsSave} className="min-w-30 mt-2 py-1 bg-amber-400 border-2
-                 border-yellow-600 hover:bg-yellow-400 rounded-md shadow-sm cursor-pointerbg-amber-400 cursor-pointer">
-                    Close
-                </button>
             </div>
-        </div>
+
+            < div className="w-100 bg-amber-100 border-4 border-yellow-700 rounded-lg shadow-lg px-4 py-3 font-press-start text-lg text-yellow-900" >
+                <h2 className="text-xl mb-2 text-center">Settings</h2>
+                <label className='flex justify-start text-sm'>
+                    <span>Dark theme</span>
+                    <div onClick={() => setIsDark(prev => !prev)} className='bg-amber-400 ml-1 w-14 h-6 border-1 border-yellow-600 rounded-xs cursor-pointer'>
+                        <div className={`w-5.5 h-5.5 bg-yellow-900 rounded-xs transition-transform duration-300 ${isDark ? 'translate-x-8' : 'translate-x-0'}`} />
+                    </div>
+                </label>
+                <div className='flex justify-center'>
+                </div>
+            </div >
+        </div >
+
     )
 
 }
--- web/src/components/Settings/SettingsWrapper.tsx
+++ web/src/components/Settings/SettingsWrapper.tsx
@@ -6,7 +6,13 @@ export default function SettingsWrapper() {
     const [isOpen, setIsOpen] = useState<boolean>(false)
     return (<div>
         <div className="flex justify-end-safe">
-            <SettingsButton handleOnClick={() => setIsOpen(true)} />
+            <SettingsButton handleOnClick={() => setIsOpen(true)} >
+                <svg fill="currentColor" className=" w-7 h-7 text-yellow-900" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier"></g>
+                    <g id="SVGRepo_tracerCarrier"></g>
+                    <g id="SVGRepo_iconCarrier">
+                        <path d="M1703.534 960c0-41.788-3.84-84.48-11.633-127.172l210.184-182.174-199.454-340.856-265.186 88.433c-66.974-55.567-143.323-99.389-223.85-128.415L1158.932 0h-397.78L706.49 269.704c-81.43 29.138-156.423 72.282-223.962 128.414l-265.073-88.32L18 650.654l210.184 182.174C220.39 875.52 216.55 918.212 216.55 960s3.84 84.48 11.633 127.172L18 1269.346l199.454 340.856 265.186-88.433c66.974 55.567 143.322 99.389 223.85 128.415L761.152 1920h397.779l54.663-269.704c81.318-29.138 156.424-72.282 223.963-128.414l265.073 88.433 199.454-340.856-210.184-182.174c7.793-42.805 11.633-85.497 11.633-127.285m-743.492 395.294c-217.976 0-395.294-177.318-395.294-395.294 0-217.976 177.318-395.294 395.294-395.294 217.977 0 395.294 177.318 395.294 395.294 0 217.976-177.317 395.294-395.294 395.294" fillRule="evenodd"></path>
+                    </g></svg>
+            </SettingsButton>
         </div>
         {
             isOpen && (<>
@@ -14,7 +20,7 @@ export default function SettingsWrapper() {
                     className="fixed inset-0 bg-[rgba(0,0,0,0.3)]  backdrop-blur-sm z-40 transition-colors duration-200"
                 />
                 <div className='flex justify-center items-center'>
-                    <SettingsWindow onSettingsSave={() => setIsOpen(false)} />
+                    <SettingsWindow onSettingsClose={() => setIsOpen(false)} />
                 </div>
             </>)
         }
